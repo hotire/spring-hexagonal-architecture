5daeabf2d997f0b13751e48dbbe98bc6e73fc70f
hotire <gngh0101@gmail.com>
2021-09-18 14:47:26 +0900

    Move to AccountPersistenceAdapter

--- src/main/kotlin/com/github/hotire/spring/hexagonal/architecture/ex/account/persistence/adapter/AccountPersistenceAdapter.kt
+++ src/main/kotlin/com/github/hotire/spring/hexagonal/architecture/ex/account/persistence/adapter/AccountPersistenceAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.hotire.spring.hexagonal.architecture.ex.account.persistence.adapter
 
+import com.github.hotire.spring.hexagonal.architecture.ex.account.CustomPropertyChangeListener
 import com.github.hotire.spring.hexagonal.architecture.ex.account.persistence.AccountEntity
 import com.github.hotire.spring.hexagonal.architecture.ex.account.persistence.repository.AccountRepository
 import com.github.hotire.spring.hexagonal.architecture.ex.account.service.domain.Account
@@ -8,6 +9,7 @@ import com.github.hotire.spring.hexagonal.architecture.ex.account.service.domain
 import com.github.hotire.spring.hexagonal.architecture.ex.account.service.port.LoadAccountPort
 import com.github.hotire.spring.hexagonal.architecture.ex.account.service.port.UpdateAccountPort
 import org.springframework.stereotype.Service
+import java.beans.PropertyChangeSupport
 
 @Service
 class AccountPersistenceAdapter(private val accountRepository: AccountRepository) :
@@ -20,6 +22,38 @@ class AccountPersistenceAdapter(private val accountRepository: AccountRepository
     }
 
     override fun updateAccount(account: Account) {
+        val origin = loadAccount(accountId = account.id)
+        val propertyChangeSupport = PropertyChangeSupport(origin)
+
+        customPropertyChangeListeners().forEach {
+            propertyChangeSupport.addPropertyChangeListener(
+                it.propertyName,
+                it
+            )
+        }
+        customPropertyChangeListeners()
+            .distinctBy { it.propertyName }
+            .forEach {
+                propertyChangeSupport.firePropertyChange(
+                    it.propertyName,
+                    it.propertyProvider(origin),
+                    it.propertyProvider(account)
+                )
+            }
+
         accountRepository.save(AccountEntity(account.id.value, account.balance.value))
     }
+
+    fun customPropertyChangeListeners(): List<CustomPropertyChangeListener<Account, *>> = listOf(object :
+            CustomPropertyChangeListener<Account, Money> {
+            override val propertyName: String
+                get() = "balance"
+            override val propertyProvider: Account.() -> Money
+                get() = { this.balance }
+
+            override fun propertyChange(oldValue: Money?, newValue: Money?) {
+                println(oldValue)
+                println(newValue)
+            }
+        })
 }
