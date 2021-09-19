04ef75fe1f122e3c27085a0762f5b6b4218de9d7
hotire <gngh0101@gmail.com>
2021-09-19 19:25:54 +0900

    Add comment

--- src/main/kotlin/com/github/hotire/spring/hexagonal/architecture/ex/account/service/port/Port.kt
+++ src/main/kotlin/com/github/hotire/spring/hexagonal/architecture/ex/account/service/port/Port.kt
@@ -2,11 +2,28 @@ package com.github.hotire.spring.hexagonal.architecture.ex.account.service.port
 
 import com.github.hotire.spring.hexagonal.architecture.ex.account.service.domain.Account
 import com.github.hotire.spring.hexagonal.architecture.ex.account.service.domain.AccountId
+import com.github.hotire.spring.hexagonal.architecture.ex.account.service.domain.Money
 
+/**
+ * output
+ */
 interface LoadAccountPort {
     fun loadAccount(accountId: AccountId): Account
 }
 
 interface UpdateAccountPort {
     fun updateAccount(account: Account)
+}
+
+/**
+ * input
+ */
+interface SendMoneyUseCase {
+    fun sendMoney(command: SendMoneyCommand): Boolean
+
+    data class SendMoneyCommand(
+        val sourceAccountId: AccountId,
+        val targetAccountId: AccountId,
+        val money: Money
+    )
 }
